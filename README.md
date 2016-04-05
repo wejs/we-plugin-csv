54f160cae2f85d1435b863ef94984a59bff2d5b4
albertosouza <alberto.souza.99@gmail.com>
2016-04-05 06:14:45 -0300

    update to add one custom response type and update the README.md

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
 # we-plugin-csv
 
-> We.js plugin do add ``we.scv`` attribute with https://github.com/wdavidw/node-csv module
+> We.js plugin do add csv response type and set ``we.csv`` attribute with https://github.com/wdavidw/node-csv module
+
+# Configuration
+
+## For responses related to model
+
+file: config/local.js
+```
+    csv: {
+      // configure your model coluns with this
+      modelColumns: {
+        // Example for user model:
+        user: {
+           userId: 'id',
+           displayName: 'displayName',
+           fullName: 'fullName',
+           biography: 'biography',
+           gender: 'gender'
+        }
+      }
+    }
+```
+
+## For configure csv columns in controller:
+
+```js
+// in your controller
+res.locals.csvResponseColumns = {
+    userId: 'id',
+    displayName: 'displayName',
+    fullName: 'fullName',
+    biography: 'biography',
+    status: 'gender'
+};
+```
 
 # API
+
+## Use the responseType API
+
+```
+http://localhost:4000/user.csv
+```
+
+## Lib
 
 > See https://github.com/wdavidw/node-csv how will be avaible in ``we.csv``
 
@@ -11,6 +53,6 @@
 
 > * We.js site: http://wejs.org
 
-#Copyright and license
+# License
 
-Copyright 2013-2015 Alberto Souza <alberto.souza.dev@gmail.com> and contributors , under [the MIT license](LICENSE).
+Under [the MIT license](LICENSE.md).
--- plugin.js
+++ plugin.js
@@ -7,9 +7,59 @@ var csv = require('csv');
 
 module.exports = function loadPlugin(projectPath, Plugin) {
   var plugin = new Plugin(__dirname);
-  // set we.csv after load all plugins
-  plugin.events.on('we:after:load:plugins', function (we) {
-    we.csv = csv;
+  plugin.setConfigs({
+    csv: {
+      // configure your model coluns with this
+      modelColumns: {
+        // // Example:
+        // user: {
+        //   displayName: 'displayName',
+        //   fullName: 'fullName',
+        //   biography: 'biography',
+        //   gender: 'gender'
+        // }
+      }
+    }
   });
+
+  plugin.csvResponseFormat = function csvResponseFormat (req, res){
+    var we = req.we;
+
+    var columns, records;
+
+    if (Array.isArray(res.locals.data)) {
+      records = res.locals.data
+    } else {
+      records = [ res.locals.data ];
+    }
+
+    // get columns:
+    if (res.locals.csvResponseColumns) {
+      columns = res.locals.csvResponseColumns;
+    } else if (res.locals.model && we.config.csv.modelColumns[res.locals.model]) {
+      columns = we.config.csv.modelColumns[res.locals.model];
+    } else {
+      // try to use the first object attr, this fails for attrs with arrays and objects
+      columns = Object.keys(res.locals.data[0].toJSON());
+    }
+
+    req.we.csv.stringify(records,{
+      header: true,
+      quotedString: true,
+      columns: columns
+    }, function afterParseDataForSendInCSV (err, data){
+      if (err) return res.serverError();
+
+      res.set('Content-Type', 'text/csv');
+      res.send(data);
+    });
+  }
+
+  // set we.csv after load all plugins
+  plugin.events.on('we:after:load:plugins', function afterLoadCSVPluginAfterLoadPlugins (we){
+    we.csv = csv;
+    we.responses.addResponseFormater('csv', plugin.csvResponseFormat);
+  });
+
   return plugin;
 };
